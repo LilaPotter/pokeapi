1f2f95874edca9cddfdf8021b9a6cc01b4cd2d96
LilaPotter <Karla.anchondo@gmail.com>
2023-06-07 20:31:59 -0600

    feat: importar httpscliente y ReactiveForms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokedexComponent } from './views/pokedex/pokedex.component';
+import { PokemoncardComponent } from './components/pokemoncard/pokemoncard.component';
+import { PokemonDescriptionComponent } from './views/pokemon-description/pokemon-description.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokedexComponent,
+    PokemoncardComponent,
+    PokemonDescriptionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
