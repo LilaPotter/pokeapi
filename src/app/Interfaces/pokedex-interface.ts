be8cadbb6a6e87859fcdc0835972b9eddd33fde5
LilaPotter <Karla.anchondo@gmail.com>
2023-06-07 20:30:13 -0600

    feat: interfaces para descripcion de pokemones

---
+++ src/app/Interfaces/pokedex-interface.ts
@@ -0,0 +1,11 @@
+export interface PokedexInterface {
+    count:    number;
+    next:     string;
+    previous: null;
+    results:  ResultInterface[];
+}
+
+export interface ResultInterface {
+    name: string;
+    url:  string;
+}
