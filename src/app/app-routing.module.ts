f8ef84aa7f36331fabb0981ca7d4a9a3a916897f
LilaPotter <Karla.anchondo@gmail.com>
2023-06-07 20:31:01 -0600

    feat: se agregaron rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokedexComponent } from './views/pokedex/pokedex.component';
+import { PokemonDescriptionComponent } from './views/pokemon-description/pokemon-description.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pokedex', component:PokedexComponent },
+  { path: 'pokemon-description/:name', component:PokemonDescriptionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
